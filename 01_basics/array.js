332097c2a50243c23e7e572507f1a56e28298651
nanwani-karan <nanwanik21@gmail.com>
2025-05-26 18:55:23 +0000

    array 2

---
+++ 01_basics/array.js
@@ -0,0 +1,10 @@
+console.log(Array.isArray("karan"));
+console.log(Array.from("hitesh"));
+console.log(Array.from({name:"karan"}));//intresting
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1,score2,score3));
+
