74730a1d21ba8fe8b7c37af20a34c6cd79bde27a
nanwani-karan <nanwanik21@gmail.com>
2025-05-27 11:03:18 +0000

    object2

---
+++ 01_basics/object2.js
@@ -0,0 +1,12 @@
+const instauser = {}
+
+instauser.id ="123abc"
+instauser.name ="karan"
+instauser.isloggedin = false
+//console.log(instauser);
+const obj1 = {1:"a" ,2:"b"}
+const obj2 = {3:"c" ,4:"d"}
+const obj3 = {5:"e" ,6:"f"}
+
+const obj4 = Object.assign({},obj1,obj2,obj3)
+//console.log(obj4);
