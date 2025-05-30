d6c4a70f4df06b6dd16ed351bd1f8ddc18709597
nanwani-karan <nanwanik21@gmail.com>
2025-05-30 03:54:30 +0000

    function

---
+++ 01_basics/function.js
@@ -0,0 +1,20 @@
+function loginusermessage(username){
+    if(username==undefined){
+        console.log(("please enter a username"));
+        return;
+        
+    }
+    return `$(karan)just logged in`
+}
+//console.log(loginusermessage("karan"));
+function calculatecartprice(...num1){
+    return num1
+}
+//console.log(calculatecartprice(200,400,500,1450,150));
+handleobject({
+    username:"sam",
+    price:399
+})
+function handleobject(anyobject){
+//console.log(`username is ${anyobject.username}  and price is ${anyobject.price}`);
+}
