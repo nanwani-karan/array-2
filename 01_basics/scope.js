963e0e1a44caac2ecc4e7e3839cd9a1171b402a7
nanwani-karan <nanwanik21@gmail.com>
2025-05-30 06:06:02 +0000

    scope

---
+++ 01_basics/scope.js
@@ -0,0 +1,22 @@
+function one() {
+    const username ="hitesh"
+    function two (){
+        const website ="youtube"
+        console.log (username);
+
+    }
+    two()
+}
+//one()
+if(true){
+    const username ="hitesh"
+    if(username==="hitesh"){
+        const website ="youtube"
+        //console.log(username+website);
+        
+    }
+}
+console.log(addone(5));
+function addone(num){
+    return num+1
+}
